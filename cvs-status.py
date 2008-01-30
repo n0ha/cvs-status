4b92d5b157a2a65ef43446f25522e0ee013465f4
viktor.lieskovsky <viktor.lieskovsky@fcfee352-133c-0410-9b55-9b56298ac40f>
2008-01-30 01:11:34 +0000

    refactored, fixed non-working-cvs case, added testing mode
    git-svn-id: https://javaflight-code.googlecode.com/svn/trunk/cvs-status@21 fcfee352-133c-0410-9b55-9b56298ac40f

--- cvs-status.py
+++ cvs-status.py
@@ -1,47 +1,55 @@
 #!/usr/bin/python
 
-""" 
+"""
 		Extract and print a list of files that are out-of-sync with CVS server.
-		Uses 'cvs status' command. Assumes user is logged in to
-		CVS, and current working dir is set to the root of a CVS
-		working dir.
-		
+		Uses 'cvs status' command. Expects user to be logged in to
+		CVS, and current working dir to be the root of a CVS working dir.
+
 		@author Viktor Lieskovsky
+		@url http://javaflight.blogspot.com
 """
 
 import re
-import popen2
+from popen2 import Popen3
 
-chunk_regex = re.compile("""
+file_status = re.compile("""
 	^               # beginning of string
 	File:\s?        # static text
 	([^\s]*)        # filename
 	\s*Status:\s?   # static text
 	(.*)            # status
 	$               # eol
-""", re.VERBOSE) 
+""", re.VERBOSE)
 
-def filter_content(fh):
-	return [line for line in fh.readlines() if line[0] != '?' and "cvs status:" not in line]
-		
-def file_chunks(status_lines):
-	if "[status aborted]" in status_lines[0]:
-		raise EnvironmentError(status_lines[0])
-	while len(status_lines) > 0:
-		result = status_lines[1]
-		del status_lines[0:9]
-		yield(result)
+def __filter_spam(content):
+	return [line for line in content if line[0] != '?' and "cvs status:" not in line]
 
-def filter_changed(status_lines):		
-	return [chunk_regex.search(chunk).groups() for chunk in file_chunks(status_lines) if not "Up-to-date" in chunk]
+def file_chunks(content):
+	while len(content) > 0:
+		chunk = content[0:9]
+		del content[0:9]
+		yield(chunk)
 
-def print_changed(changed):
-	text = "File: %s%sStatus: %s"
+def filter_unchanged(content):
+	chunks = file_chunks(__filter_spam(content.readlines()))
+	return [file_status.search(chunk[1]).groups() for chunk in chunks if not "Up-to-date" in chunk[1]]
+
+def report(changed):
+	file_message = "File: %s%sStatus: %s"
 	column_filling = lambda filename: (50 - len(filename)) * " "
-	print "\n".join([text % (f[0], column_filling(f[0]), f[1]) for f in changed])
+	print "\n".join([file_message % (f[0], column_filling(f[0]), f[1]) for f in changed])
 	print "\n", ("There are %s out-of-sync files in your working tree." % len(changed)), "\n"
 
+def get_cvs_output():
+	p = Popen3('cvs status')
+	if p.wait():
+		raise EnvironmentError
+	return p.fromchild
+
 if __name__ == '__main__':
-	exec_cmd = popen2.popen4('cvs status')
-	content = filter_content(exec_cmd[0])
-	print_changed(filter_changed(content))
+	try:
+		test = False
+		fh = test and open('cvs-status.log') or get_cvs_output()
+		report(filter_unchanged(fh))
+	except (EnvironmentError, IOError), message:
+		print message
